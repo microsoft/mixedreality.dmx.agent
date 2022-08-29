ab570d54bc1ee258fb827340a0f748d510f8270c
Terry Palomares <tpalomares18@gmail.com>
2022-08-29 12:05:24 -0700

    CODE RUB: Standardize code

--- DMX.Agent.Worker/Program.cs
+++ DMX.Agent.Worker/Program.cs
@@ -1,10 +1,14 @@
+// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
 using DMX.Agent.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+        services.AddHostedService<Worker>())
+            .Build();
 
 host.Run();
--- DMX.Agent.Worker/Worker.cs
+++ DMX.Agent.Worker/Worker.cs
@@ -1,13 +1,21 @@
+// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 namespace DMX.Agent.Worker
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
 
-        public Worker(ILogger<Worker> logger)
-        {
+        public Worker(ILogger<Worker> logger) =>
             _logger = logger;
-        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
