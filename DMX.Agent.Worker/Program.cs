33e94c9024eaac2f62029b8915ef0c44a73ababe
Terry Palomares <tpalomares18@gmail.com>
2022-08-29 11:35:14 -0700

    INFRA: Initialize Dmx Agent Worker

---
+++ DMX.Agent.Worker/Program.cs
@@ -0,0 +1,10 @@
+using DMX.Agent.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
