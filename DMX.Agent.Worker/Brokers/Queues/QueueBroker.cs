cf7db182cac1365b5ee6909eb93c183865b92a44
Terry Palomares <tpalomares18@gmail.com>
2022-10-24 10:26:24 -0700

    BROKERS: QueueBroker - ListenToLabCommandsQueue

---
+++ DMX.Agent.Worker/Brokers/Queues/QueueBroker.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace DMX.Agent.Worker.Brokers.Queues
+{
+    public partial class QueueBroker : IQueueBroker
+    {
+        private readonly string serviceBusConnection;
+
+        public QueueBroker(string serviceBusConnection)
+        {
+            this.serviceBusConnection = serviceBusConnection;
+            InitializeQueue();
+        }
+
+        private void InitializeQueue() =>
+            this.labCommandQueue = GetQueueClient(nameof(this.labCommandQueue));
+
+        private MessageHandlerOptions GetMessageHandlerOptions()
+        {
+            return new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                AutoComplete = false,
+                MaxConcurrentCalls = 1
+            };
+        }
+
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            ExceptionReceivedContext context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+
+            return Task.CompletedTask;
+        }
+
+        private IQueueClient GetQueueClient(string queueName) =>
+            new QueueClient(this.serviceBusConnection, queueName);
+    }
+}
