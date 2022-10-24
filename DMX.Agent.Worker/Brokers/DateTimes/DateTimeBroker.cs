2beca57b34b21ea66b7346ef398d2ec1c1ffe707
Geetika Jain <geetsk2194@gmail.com>
2022-10-24 11:03:37 -0700

    BROKERS: Date time broker

---
+++ DMX.Agent.Worker/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace DMX.Agent.Worker.Brokers.DateTimes
+{
+    public class DateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
