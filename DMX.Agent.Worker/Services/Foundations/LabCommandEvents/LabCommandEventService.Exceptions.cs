930a66e014d686edb14051fbede242f1569dd2bd
Terry Palomares <tpalomares18@gmail.com>
2022-10-25 13:18:22 -0700

    ShouldThrowCriticalDependencyExceptionOnListenAndLogItAsync -> PASS

--- DMX.Agent.Worker/Services/Foundations/LabCommandEvents/LabCommandEventService.Exceptions.cs
+++ DMX.Agent.Worker/Services/Foundations/LabCommandEvents/LabCommandEventService.Exceptions.cs
@@ -4,6 +4,7 @@
 
 using System;
 using DMX.Agent.Worker.Models.LabCommands.Exceptions;
+using Microsoft.Azure.ServiceBus;
 using Xeptions;
 
 namespace DMX.Agent.Worker.Services.Foundations.LabCommandEvents
@@ -18,12 +19,41 @@ namespace DMX.Agent.Worker.Services.Foundations.LabCommandEvents
             {
                 returningNothingFunction();
             }
+            catch (MessagingEntityNotFoundException messagingEntityNotFoundException)
+            {
+                var failedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(messagingEntityNotFoundException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabCommandDependencyException);
+            }
+            catch (MessagingEntityDisabledException messagingEntityDisabledException)
+            {
+                var failedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(messagingEntityDisabledException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabCommandDependencyException);
+            }
+            catch (UnauthorizedAccessException unauthorizedAccessException)
+            {
+                var failedLabCommandDependencyException =
+                    new FailedLabCommandDependencyException(unauthorizedAccessException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabCommandDependencyException);
+            }
             catch (Exception exception)
             {
                 var failedLabCommandServiceException = new FailedLabCommandServiceException(exception);
 
                 throw CreateAndLogServiceException(failedLabCommandServiceException);
             }
+        }
+
+        private Exception CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var labCommandDependencyException = new LabCommandDependencyException(exception);
+            this.loggingBroker.LogCritical(labCommandDependencyException);
+
+            return labCommandDependencyException;
         }
 
         private Xeption CreateAndLogServiceException(Xeption exception)
