63a9ec2dceef24d5112164c2dc94c6b9913551f9
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-10-26 14:15:41 -0400

    ShouldModifyLabWorkflowCommandAsync -> FAIL

---
+++ DMX.Agent.Worker.Tests.Unit/Services/Foundations/LabWorkflowCommands/LabWorkflowCommandServiceTests.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using DMX.Agent.Worker.Brokers.DateTimes;
+using DMX.Agent.Worker.Brokers.DmxApis;
+using DMX.Agent.Worker.Brokers.Loggings;
+using DMX.Agent.Worker.Models.Foundations.LabWorkflowCommands;
+using DMX.Agent.Worker.Services.Foundations.LabWorkflowCommands;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace DMX.Agent.Worker.Tests.Unit.Services.Foundations.LabWorkflowCommands
+{
+    public partial class LabWorkflowCommandServiceTests
+    {
+        private readonly Mock<IDmxApiBroker> dmxApiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ILabWorkflowCommandService labWorkflowCommandService;
+
+        public LabWorkflowCommandServiceTests()
+        {
+            this.dmxApiBrokerMock = new Mock<IDmxApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.labWorkflowCommandService = new LabWorkflowCommandService(
+                dmxApiBroker: this.dmxApiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static LabWorkflowCommand CreateRandomLabWorkflowCommand() =>
+            CreateLabWorkflowCommandFiller(GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<LabWorkflowCommand> CreateLabWorkflowCommandFiller(DateTimeOffset dateTimeOffset)
+        {
+            var filler = new Filler<LabWorkflowCommand>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>()
+                    .Use(dateTimeOffset);
+
+            return filler;
+        }
+    }
+}
---
+++ DMX.Agent.Worker/Services/Foundations/LabWorkflowCommands/LabWorkflowCommandService.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using DMX.Agent.Worker.Brokers.DmxApis;
+using DMX.Agent.Worker.Brokers.Loggings;
+using DMX.Agent.Worker.Models.Foundations.LabWorkflowCommands;
+
+namespace DMX.Agent.Worker.Services.Foundations.LabWorkflowCommands
+{
+    public partial class LabWorkflowCommandService : ILabWorkflowCommandService
+    {
+        private readonly IDmxApiBroker dmxApiBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public LabWorkflowCommandService(
+            IDmxApiBroker dmxApiBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.dmxApiBroker = dmxApiBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<LabWorkflowCommand> ModifyLabWorkflowCommandAsync(LabWorkflowCommand labWorkflowCommand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
