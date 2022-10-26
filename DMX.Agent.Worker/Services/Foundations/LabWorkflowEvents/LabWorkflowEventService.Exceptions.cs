eef35d262e2318be511737442ddd7b6e24b37449
Michael Mendelsohn <jazzapple42@gmail.com>
2022-10-26 13:31:57 -0700

    ShouldThrowCriticalDependencyExceptionOnListenAndLogItAsync -> PASS

--- DMX.Agent.Worker/Services/Foundations/LabWorkflowEvents/LabWorkflowEventService.Exceptions.cs
+++ DMX.Agent.Worker/Services/Foundations/LabWorkflowEvents/LabWorkflowEventService.Exceptions.cs
@@ -4,6 +4,7 @@
 
 using System;
 using DMX.Agent.Worker.Models.LabWorkflows.Exceptions;
+using Microsoft.Azure.ServiceBus;
 using Xeptions;
 
 namespace DMX.Agent.Worker.Services.Foundations.LabWorkflowEvents
@@ -22,11 +23,44 @@ namespace DMX.Agent.Worker.Services.Foundations.LabWorkflowEvents
             {
                 throw CreateAndLogValidationException(nullLabWorkflowHandlerException);
             }
+            catch (MessagingEntityNotFoundException messagingEntityNotFoundException)
+            {
+                var failedLabWorkflowDependencyException =
+                    new FailedLabWorkflowDependencyException(messagingEntityNotFoundException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabWorkflowDependencyException);
+            }
+            catch (MessagingEntityDisabledException messagingEntityDisabledException)
+            {
+                var failedLabWorkflowDependencyException =
+                    new FailedLabWorkflowDependencyException(messagingEntityDisabledException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabWorkflowDependencyException);
+            }
+            catch (UnauthorizedAccessException unauthorizedAccessException)
+            {
+                var failedLabWorkflowDependencyException =
+                    new FailedLabWorkflowDependencyException(unauthorizedAccessException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabWorkflowDependencyException);
+            }
+        }
+
+        private LabWorkflowDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var labWorkflowDependencyException =
+                new LabWorkflowDependencyException(exception);
+
+            this.loggingBroker.LogCritical(labWorkflowDependencyException);
+
+            return labWorkflowDependencyException;
         }
 
         private LabWorkflowValidationException CreateAndLogValidationException(Xeption exception)
         {
-            var labWorkflowValidationException = new LabWorkflowValidationException(exception);
+            var labWorkflowValidationException =
+                new LabWorkflowValidationException(exception); 
+
             this.loggingBroker.LogError(labWorkflowValidationException);
 
             return labWorkflowValidationException;
