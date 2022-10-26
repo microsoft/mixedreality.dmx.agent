85d83d6bc842b192229188db2b97eebfeaff9821
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-10-26 15:42:58 -0400

    ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync -> PASS

--- DMX.Agent.Worker/Services/Foundations/LabWorkflowCommands/LabWorkflowCommandService.Exceptions.cs
+++ DMX.Agent.Worker/Services/Foundations/LabWorkflowCommands/LabWorkflowCommandService.Exceptions.cs
@@ -5,6 +5,7 @@
 using System.Threading.Tasks;
 using DMX.Agent.Worker.Models.Foundations.LabWorkflowCommands;
 using DMX.Agent.Worker.Models.Foundations.LabWorkflowCommands.Exceptions;
+using RESTFulSense.Exceptions;
 using Xeptions;
 
 namespace DMX.Agent.Worker.Services.Foundations.LabWorkflowCommands
@@ -27,6 +28,27 @@ namespace DMX.Agent.Worker.Services.Foundations.LabWorkflowCommands
             {
                 throw CreateAndLogValidationException(invalidLabWorkflowCommandException);
             }
+            catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+            {
+                var failedLabWorkflowCommandDependencyException =
+                    new FailedLabWorkflowCommandDependencyException(httpResponseUrlNotFoundException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabWorkflowCommandDependencyException);
+            }
+            catch (HttpResponseUnauthorizedException httpResponseUnauthorizedException)
+            {
+                var failedLabWorkflowCommandDependencyException =
+                    new FailedLabWorkflowCommandDependencyException(httpResponseUnauthorizedException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabWorkflowCommandDependencyException);
+            }
+            catch (HttpResponseForbiddenException httpResponseForbiddenException)
+            {
+                var failedLabWorkflowCommandDependencyException =
+                    new FailedLabWorkflowCommandDependencyException(httpResponseForbiddenException);
+
+                throw CreateAndLogCriticalDependencyException(failedLabWorkflowCommandDependencyException);
+            }
         }
 
         private LabWorkflowCommandValidationException CreateAndLogValidationException(Xeption exception)
@@ -37,6 +59,16 @@ namespace DMX.Agent.Worker.Services.Foundations.LabWorkflowCommands
             this.loggingBroker.LogError(labWorkflowCommandValidationException);
 
             return labWorkflowCommandValidationException;
+        }
+
+        private LabWorkflowCommandDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var labWorkflowCommandDependencyException =
+                new LabWorkflowCommandDependencyException(exception);
+
+            this.loggingBroker.LogCritical(labWorkflowCommandDependencyException);
+
+            return labWorkflowCommandDependencyException;
         }
     }
 }
