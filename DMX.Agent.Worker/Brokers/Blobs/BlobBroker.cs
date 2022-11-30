13c4b58f584fe7d1eea4ada7ca465b78f0404835
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-11-30 11:44:13 -0500

    CODE RUB: Reorder Properties

--- DMX.Agent.Worker/Brokers/Blobs/BlobBroker.cs
+++ DMX.Agent.Worker/Brokers/Blobs/BlobBroker.cs
@@ -10,19 +10,19 @@ namespace DMX.Agent.Worker.Brokers.Blobs
 {
     public partial class BlobBroker : IBlobBroker
     {
-        private readonly BlobServiceClient blobServiceClient;
-        private readonly BlobContainerClient blobContainerClient;
         private readonly IConfiguration configuration;
         private readonly string connectionString;
         private readonly string containerName;
+        private readonly BlobServiceClient blobServiceClient;
+        private readonly BlobContainerClient blobContainerClient;
 
         public BlobBroker(IConfiguration configuration)
         {
             this.configuration = configuration;
-            connectionString = GetStorageConnectionString(this.configuration);
-            containerName = GetContainerName(this.configuration);
-            blobServiceClient = new BlobServiceClient(connectionString);
-            blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            this.connectionString = GetStorageConnectionString(this.configuration);
+            this.containerName = GetContainerName(this.configuration);
+            this.blobServiceClient = new BlobServiceClient(connectionString);
+            this.blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
         }
 
         private static string GetStorageConnectionString(IConfiguration configuration)
