b32ef4998632d6fe4c66e5d63362bc4a92676240
Geetika Jain <geetsk2194@gmail.com>
2023-01-17 04:23:37 -0800

    ShouldThrowValidationExceptionOnDownloadIfArtifactFilePathIsNullAndLogItAsync() -> FAIL

--- DMX.Agent.Worker.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Validations.Download.cs
+++ DMX.Agent.Worker.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Validations.Download.cs
@@ -55,5 +55,48 @@ namespace DMX.Agent.Worker.Tests.Unit.Services.Foundations.Artifacts
             this.ArtifactBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public async Task ShouldThrowValidationExceptionOnDownloadIfArtifactFilePathIsNullAndLogItAsync(string invalidString)
+        {
+            // given
+            string someArtifactName = GetRandomString();
+            string nullFilePath = invalidString;
+
+            EmptyArtifactFilePathException emptyArtifactFilePathException =
+                new EmptyArtifactFilePathException();
+
+            ArtifactValidationException expectedArtifactValidationException =
+                new ArtifactValidationException(emptyArtifactFilePathException);
+
+            // when
+            ValueTask<Response> downloadArtifactTask =
+                this.ArtifactService.DownloadArtifactAsync(someArtifactName, nullFilePath);
+
+            ArtifactValidationException actualArtifactValidationException =
+                await Assert.ThrowsAsync<ArtifactValidationException>(
+                    downloadArtifactTask.AsTask);
+
+            // then
+            actualArtifactValidationException.Should().BeEquivalentTo(
+                expectedArtifactValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedArtifactValidationException))),
+                        Times.Once);
+
+            this.ArtifactBrokerMock.Verify(broker =>
+                broker.DownloadLabArtifactToFilePathAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                        Times.Never);
+
+            this.ArtifactBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ DMX.Agent.Worker/Models/Foundations/Artifacts/Exceptions/EmptyArtifactFilePathException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Agent.Worker.Models.Foundations.Artifacts.Exceptions
+{
+    public class EmptyArtifactFilePathException : Xeption
+    {
+        public EmptyArtifactFilePathException()
+            : base(message: "Artifact file path is empty. Please try again.")
+        { }
+    }
+}
