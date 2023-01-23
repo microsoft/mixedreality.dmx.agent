e234bbd441efc97d8ceb6a009e1c41f364bbee6c
Michael Mendelsohn <jazzapple42@gmail.com>
2023-01-23 11:27:33 -0800

    ShouldThrowCriticalDependencyExceptionOnDownloadIfCriticalErrorOccursAndLogItAsync -> FAIL

--- DMX.Agent.Worker.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Exceptions.Download.cs
+++ DMX.Agent.Worker.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Exceptions.Download.cs
@@ -7,6 +7,7 @@ using DMX.Agent.Worker.Models.Foundations.Artifacts.Exceptions;
 using FluentAssertions;
 using Moq;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -22,7 +23,6 @@ namespace DMX.Agent.Worker.Tests.Unit.Services.Foundations.Artifacts
             // given
             string someFilePath = GetRandomString();
             string someArtifactName = GetRandomString();
-            Response someResponse = new Mock<Response>().Object;
 
             FailedArtifactDependencyException failedArtifactDependencyException =
                 new FailedArtifactDependencyException(dependencyException);
@@ -54,6 +54,59 @@ namespace DMX.Agent.Worker.Tests.Unit.Services.Foundations.Artifacts
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
+                    expectedArtifactDependencyException))),
+                        Times.Once());
+
+            this.ArtifactBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData((int)HttpStatusCode.Unauthorized)]
+        [InlineData((int)HttpStatusCode.Forbidden)]
+        [InlineData((int)HttpStatusCode.NotFound)]
+        public async Task ShouldThrowCriticalDependencyExceptionOnDownloadIfCriticalErrorOccursAndLogItAsync(
+    int crititicalStatusCode)
+        {
+            // given
+            string randomMessage = GetRandomString();
+            string someFilePath = GetRandomString();
+            string someArtifactName = GetRandomString();
+
+            var requestFailedException =
+                new RequestFailedException(
+                    crititicalStatusCode,
+                    randomMessage);
+
+            var failedArtifactDependencyException =
+                new FailedArtifactDependencyException(requestFailedException);
+
+            var expectedArtifactDependencyException =
+                new ArtifactDependencyException(failedArtifactDependencyException);
+
+            this.ArtifactBrokerMock.Setup(broker =>
+                broker.DownloadLabArtifactToFilePathAsync(
+                    It.IsAny<string>(), It.IsAny<string>()))
+                        .Throws(requestFailedException);
+
+            // when
+            ValueTask<Response> downloadArtifactTask =
+                this.ArtifactService.DownloadArtifactAsync(someArtifactName, someFilePath);
+
+            ArtifactDependencyException actualArtifactDependencyException =
+                await Assert.ThrowsAsync<ArtifactDependencyException>(downloadArtifactTask.AsTask);
+
+            // then
+            actualArtifactDependencyException.Should().BeEquivalentTo(
+                expectedArtifactDependencyException);
+
+            this.ArtifactBrokerMock.Verify(broker =>
+                broker.DownloadLabArtifactToFilePathAsync(
+                    someArtifactName, someFilePath),
+                        Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
                     expectedArtifactDependencyException))),
                         Times.Once());
 
