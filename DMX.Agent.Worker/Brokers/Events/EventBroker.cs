c1308cbac2e1e403c9df76fa76f270543dc004eb
Hassan Habib <hassanhabib@live.com>
2023-02-06 10:31:58 -0800

    BROKERS: Listen to Lab Workflow Commands

---
+++ DMX.Agent.Worker/Brokers/Events/EventBroker.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using DMX.Agent.Worker.Models.Foundations.LabWorkflowCommands;
+using LeVent.Clients;
+
+namespace DMX.Agent.Worker.Brokers.Events
+{
+    public partial class EventBroker
+    {
+        public EventBroker()
+        {
+            this.LabWorkflowCommandEvents = 
+                new LeVentClient<LabWorkflowCommand>();
+        }
+    }
+}
