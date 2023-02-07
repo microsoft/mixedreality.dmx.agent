6826755e681263150abafb70b278db985520793e
Andrew McClelland <andrew.mcclelland@microsoft.com>
2023-02-07 11:36:53 -0500

    ShouldThrowValidationExceptionOnRegisterIfLabWorkflowCommandEventHandlerIsNullAndLogItAsync -> FAIL

---
+++ DMX.Agent.Worker/Models/Foundations/LabWorkflowCommandEvents/NullLabWorkflowCommandEventHandlerException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Agent.Worker.Models.Foundations.LabWorkflowCommandEvents
+{
+    public class NullLabWorkflowCommandEventHandlerException : Xeption
+    {
+        public NullLabWorkflowCommandEventHandlerException()
+            : base(message: "Lab workflow command event handler is null.")
+        { }
+    }
+}
